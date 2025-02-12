93d2ea662e9fb57be44a23240edd956602312e88
adil <adilsultanbaev43@gmail.com>
2025-02-12 14:02:13 +0600

    1.2-5 js cod

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-let number = 12345;
-let length = String(number).length;
-console.log(length);
+let number1 = 12345;
+let number2 = 16789;
+let firstDigit1 = String(number1)[0];
+let firstDigit2 = String(number2)[0];
+console.log(firstDigit1 === firstDigit2);
