20a5cd5fd12e93a9a78c14fd56b71267b94160cf
adil <adilsultanbaev43@gmail.com>
2025-02-12 14:01:43 +0600

    1.2-4 js cod

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 let number = 12345;
-let strNumber = String(number);
-let firstDigit = +strNumber[0];
-let lastDigit = +strNumber.slice(-1);
-console.log(firstDigit + lastDigit);
+let length = String(number).length;
+console.log(length);
